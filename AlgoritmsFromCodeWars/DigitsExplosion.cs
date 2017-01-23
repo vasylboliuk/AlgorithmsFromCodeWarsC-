3299bfa93a312ca4cb331cf1346d7bbc844db354
vasylboliuk <vbboliuk@gmail.com>
2017-01-23 13:00:41 +0200

    Algorithms From Code Wars C#

---
+++ AlgoritmsFromCodeWars/DigitsExplosion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmsFromCodeWars
+{
+    class DigitsExplosion
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        
+        /*
+         * 
+         * 
+         */
+        public static string Explode(string s)
+        {
+            string newStr = "";
+            foreach (char str in s)
+            {
+                int num = (int)Char.GetNumericValue(str);
+                for (int i = 1; i <= num; i++)
+                {
+                    newStr = newStr + num;
+                }
+            }
+            return newStr;
+        }
+
+    }
+}
