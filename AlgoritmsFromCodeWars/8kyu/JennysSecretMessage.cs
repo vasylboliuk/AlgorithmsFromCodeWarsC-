9617eb2e99d5b4a685f8026114d22d04341a7852
vasylboliuk <vbboliuk@gmail.com>
2017-01-24 22:00:55 +0200

    Add new solution Jennys Secret Message Bug fix

---
+++ AlgoritmsFromCodeWars/8kyu/JennysSecretMessage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmsFromCodeWars._8kyu
+{
+    public class JennysSecretMessage
+    {
+        /*
+         * Description:
+         * Jenny has written a function that returns a greeting for a user.
+         *  However, she's in love with Johnny, and would like to greet him slightly different. 
+         *  She added a special case to her function, but she made a mistake.
+         *  Can you help her?
+         */
+        public static string greet(string name)
+        {
+            if (name == "Johnny")
+                return "Hello, my love!";
+            return "Hello, " + name + "!";
+           
+        }
+    }
+}
---
+++ TestsAlgoritmsFromCodeWars/8kyu/TestJennysSecretMessage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlgoritmsFromCodeWars._8kyu;
+using NUnit.Framework;
+
+namespace TestsAlgoritmsFromCodeWars._8kyu
+{
+    [TestFixture]
+    class TestJennysSecretMessage
+    {
+        [Test]
+        public static void ShouldGreetJimNormally()
+        {
+            Assert.AreEqual("Hello, Jim!", JennysSecretMessage.greet("Jim"));
+        }
+        [Test]
+        public static void ShouldGreetJaneNormally()
+        {
+            Assert.AreEqual("Hello, Jane!", JennysSecretMessage.greet("Jane"));
+        }
+        [Test]
+        public static void ShouldGreetSimonNormally()
+        {
+            Assert.AreEqual("Hello, Simon!", JennysSecretMessage.greet("Simon"));
+        }
+
+        [Test]
+        public static void ShouldGreetJohnnySpecially()
+        {
+            Assert.AreEqual("Hello, my love!", JennysSecretMessage.greet("Johnny"));
+        }
+    }
+}
