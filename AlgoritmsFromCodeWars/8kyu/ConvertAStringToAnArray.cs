14aafbe0d72191d066b780d1781dd029f331685b
vasylboliuk <vbboliuk@gmail.com>
2017-01-25 10:32:56 +0200

    Add new solution Convert a string to Array

---
+++ AlgoritmsFromCodeWars/8kyu/ConvertAStringToAnArray.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmsFromCodeWars._8kyu
+{
+    public class ConvertAStringToAnArray
+    {
+        /*
+         * Description:
+         * Write a function to split a string and convert it into an array of words.
+         * e.g.
+         * string_to_array("Robin Singh") => ["Robin", "Singh"]
+         * string_to_array("I love arrays they are my favorite") => ["I", "love", "arrays", "they", "are", "my", "favorite"]
+         * Note: in Javascript the function is written in camelCase as 'stringToArray'
+         */
+        public static string[] StringToArray(string str)
+        {
+            return str.Split().ToArray();
+        }
+    }
+}
---
+++ TestsAlgoritmsFromCodeWars/8kyu/TestConvertAStringToAnArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlgoritmsFromCodeWars._8kyu;
+using NUnit.Framework;
+
+namespace TestsAlgoritmsFromCodeWars._8kyu
+{
+    [TestFixture]
+    class TestConvertAStringToAnArray
+    {
+        [Test]
+        public void _1_Example()
+        {
+            Assert.AreEqual(new string[] { "Robin", "Singh" }, ConvertAStringToAnArray.StringToArray("Robin Singh"));
+        }
+
+        [Test]
+        public void _2_Example()
+        {
+            Assert.AreEqual(new string[] { "I", "love", "arrays", "they", "are", "my", "favorite" }, ConvertAStringToAnArray.StringToArray("I love arrays they are my favorite"));
+        }
+    }
+}
