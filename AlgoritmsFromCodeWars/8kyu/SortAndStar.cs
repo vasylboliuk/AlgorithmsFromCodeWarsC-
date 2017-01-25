6601786ca39b0d1b21a8fdb77e3aa80fcb15c2f4
vasylboliuk <vbboliuk@gmail.com>
2017-01-25 11:16:11 +0200

    Add new solution Sort and add stars to first letter in string

---
+++ AlgoritmsFromCodeWars/8kyu/SortAndStar.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace AlgoritmsFromCodeWars._8kyu
+{
+    public class SortAndStar
+    {
+        /*
+         * Description:
+         * You will be given an vector of string(s). 
+         * You must sort it alphabetically (case-sensitive!!) and then return the first value.
+         * The returned value must be a string, and have '***' between each of its letters.
+         */
+        public static string TwoSort(string[] s)
+        {
+            Array.Sort(s, StringComparer.Ordinal);
+            char[] strArr = s[0].ToCharArray();
+            string result = "";
+            for (int i = 0; i < strArr.Length; i++)
+            {
+                if (i != strArr.Length - 1)
+                    result = result + strArr[i] + "***";
+                else
+                    result = result + strArr[i];
+            }
+            return result;
+        }
+
+        //Other Solutions
+        public static string TwoSort2(string[] s)
+        {
+            return string.Join("***", s.OrderBy(a => a, StringComparer.Ordinal).First().ToArray());
+        }
+    }
+}
---
+++ TestsAlgoritmsFromCodeWars/8kyu/TestSortAndStar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlgoritmsFromCodeWars._8kyu;
+using NUnit.Framework;
+
+namespace TestsAlgoritmsFromCodeWars._8kyu
+{
+    [TestFixture]
+    class TestSortAndStar
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual("b***i***t***c***o***i***n", SortAndStar.TwoSort(new[] { "bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps" }));
+            Assert.AreEqual("a***r***e", SortAndStar.TwoSort(new[] { "turns", "out", "random", "test", "cases", "are", "easier", "than", "writing", "out", "basic", "ones" }));
+        }
+    }
+}
