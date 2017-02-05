42336b09159c898d7b318dc7b9f54c0453dabec3
vasylboliuk <vbboliuk@gmail.com>
2017-02-05 21:42:38 +0200

    Add new solution 7kyu: Sequence Sum

---
+++ AlgoritmsFromCodeWars/7kyu/SumOfASequence.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmsFromCodeWars._7kyu
+{
+    public class SumOfASequence
+    {
+        /// <summary>
+        /// Your task is to make function, which returns the sum of a sequence of integers.
+        /// The sequence is defined by 3 non-negative values: begin, end, step.
+        /// If begin value is greater than the end, function should returns 0
+        /// Examples
+        /// This is the first kata in the series:
+        /// 1) Sum of a sequence(this kata)
+        /// 2) Sum of a Sequence[Hard - Core Version]
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <param name="step"></param>
+        /// <returns></returns>
+        public static int SequenceSum(int start, int end, int step)
+        {
+            int sum = 0;
+            if (start > end) return 0;
+
+            for (int i = start; i <= end; i=i+step)
+            {
+                sum = sum + i;
+            }
+            return sum;
+        }
+        
+    }
+}
---
+++ TestsAlgoritmsFromCodeWars/7kyu/TestSumOfASequence.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlgoritmsFromCodeWars._7kyu;
+using NUnit.Framework;
+
+namespace TestsAlgoritmsFromCodeWars._7kyu
+{
+    [TestFixture]
+    class TestSumOfASequence
+    {
+
+        [Test]
+        public void BasicTest()
+        {
+            Assert.AreEqual(SumOfASequence.SequenceSum(2, 6, 2), 12);
+            Assert.AreEqual(SumOfASequence.SequenceSum(1, 5, 1), 15);
+            Assert.AreEqual(SumOfASequence.SequenceSum(1, 5, 3), 5);
+            Assert.AreEqual(SumOfASequence.SequenceSum(0, 15, 3), 45);
+            Assert.AreEqual(SumOfASequence.SequenceSum(16, 15, 3), 0);
+            Assert.AreEqual(SumOfASequence.SequenceSum(2, 24, 22), 26);
+            Assert.AreEqual(SumOfASequence.SequenceSum(2, 2, 2), 2);
+            Assert.AreEqual(SumOfASequence.SequenceSum(2, 2, 1), 2);
+            Assert.AreEqual(SumOfASequence.SequenceSum(1, 15, 3), 35);
+            Assert.AreEqual(SumOfASequence.SequenceSum(15, 1, 3), 0);
+        }
+    }
+}
