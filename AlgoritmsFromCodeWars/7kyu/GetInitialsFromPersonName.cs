36714cc197904f948336ec6dd9ab8d3a1c6959df
vasylboliuk <vbboliuk@gmail.com>
2017-02-05 16:37:05 +0200

    Add new solution 7kyu: Get Initials from person name

---
+++ AlgoritmsFromCodeWars/7kyu/GetInitialsFromPersonName.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmsFromCodeWars._7kyu
+{
+    public class GetInitialsFromPersonName
+    {
+
+        /// <summary> 
+        /// Write function toInitials returs initials for a given person name. E.g: "Bill Gates" -> "B. G."
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static string ToInitials(string name)
+        {
+            string result = "";
+            string[] names = name.Split();
+            foreach (var str in names)
+            {
+                result = result + str.Substring(0, 1) + ". ";
+            }
+            return result.Trim();
+        }
+
+
+        public static string ToInitialsLinq(string name)
+        {
+            return  string.Join(". ", name.Split().Select(x => x.Substring(0,1)))+".";
+        }
+
+        public static string ToInitialsLinq2(string name)
+        {
+            return string.Join(" ", name.Split(' ').Select(n => n.First() + "."));
+        }
+    }
+}
---
+++ TestsAlgoritmsFromCodeWars/7kyu/TestGetInitialsFromPersonName.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlgoritmsFromCodeWars._7kyu;
+using NUnit.Framework;
+
+namespace TestsAlgoritmsFromCodeWars._7kyu
+{
+    [TestFixture]
+    class TestGetInitialsFromPersonName
+    {
+        [Test]
+        public void MyTest()
+        {
+            Assert.AreEqual(GetInitialsFromPersonName.ToInitialsLinq("Kerry King"), "K. K.");
+            Assert.AreEqual(GetInitialsFromPersonName.ToInitialsLinq("Tom Araya"), "T. A.");
+            Assert.AreEqual(GetInitialsFromPersonName.ToInitialsLinq("Kerry King Kingrosovich"), "K. K. K.");
+        }
+
+    }
+}
