48be978f7423591a3abb661334244d97a5def19b
vasylboliuk <vbboliuk@gmail.com>
2017-02-05 20:24:10 +0200

    Add new solution 7kyu: Sum of all the multiples of 3 or 5

---
+++ AlgoritmsFromCodeWars/7kyu/SumOfAllTheMultiplesOf3Or5.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmsFromCodeWars._7kyu
+{
+    public class SumOfAllTheMultiplesOf3Or5
+    {
+
+        // Sum of all the multiples of 3 or 5
+        /// <summary>
+        /// Sum of all the multiples of 3 or 5
+        /// Your task is to write function findSum.
+        /// Upto and including n, this function will return the sum of all multiples of 3 and 5.
+        /// For example:
+        /// indSum(5) should return 8 (3 + 5)
+        /// findSum(10) should return 33 (3 + 5 + 6 + 9 + 10)
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static int FindSum(int n)
+        {
+            int sum = 0;
+            for (int i = 0; i <= n; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+            return sum;
+        }
+
+        public static int FindSum2(int n)
+        {
+            return Enumerable.Range(1, n).Where(x => x % 3 == 0 || x % 5 == 0).Sum();
+        }
+
+    }
+}
---
+++ TestsAlgoritmsFromCodeWars/7kyu/TestSumOfAllTheMultiplesOf3Or5.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlgoritmsFromCodeWars._7kyu;
+using NUnit.Framework;
+
+namespace TestsAlgoritmsFromCodeWars._7kyu
+{
+    [TestFixture]
+    class TestSumOfAllTheMultiplesOf3Or5
+    {
+
+        [Test]
+        public void MyTest()
+        {
+            Assert.AreEqual(8, SumOfAllTheMultiplesOf3Or5.FindSum(5));
+            Assert.AreEqual(33, SumOfAllTheMultiplesOf3Or5.FindSum(10));
+        }
+    }
+}
