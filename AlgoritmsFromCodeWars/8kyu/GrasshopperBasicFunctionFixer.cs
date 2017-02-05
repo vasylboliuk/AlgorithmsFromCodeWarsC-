d41736a387f08afcac58130ffbded5d43a76415b
vasylboliuk <vbboliuk@gmail.com>
2017-02-05 15:13:56 +0200

    Add new solution 8kyu: Grasshopper Basic Function fixer

---
+++ AlgoritmsFromCodeWars/8kyu/GrasshopperBasicFunctionFixer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmsFromCodeWars._8kyu
+{
+    public class GrasshopperBasicFunctionFixer
+    {
+        /// <summary>
+        /// Fix the function
+        /// I created this function to add five to any number that was passed in to it and return the new value.
+        /// It doesn't throw any errors but it returns the wrong number.
+        /// Can you help me fix the function?
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static int AddFive(int num)
+        {
+            int total = num + 5;
+            return total;
+        }
+
+    }
+}
---
+++ TestsAlgoritmsFromCodeWars/8kyu/TestGrasshopperBasicFunctionFixer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlgoritmsFromCodeWars._8kyu;
+using NUnit.Framework;
+
+namespace TestsAlgoritmsFromCodeWars._8kyu
+{
+    [TestFixture]
+    class TestGrasshopperBasicFunctionFixer
+    {
+        [Test]
+        public void FixedTest()
+        {
+            Assert.AreEqual(6, GrasshopperBasicFunctionFixer.AddFive(1));
+            Assert.AreEqual(20, GrasshopperBasicFunctionFixer.AddFive(15));
+            Assert.AreEqual(5, GrasshopperBasicFunctionFixer.AddFive(0));
+           
+        }
+    }
+}
