b4cbbb1a235976d107b274f31102e4aa64984c82
vasylboliuk <vbboliuk@gmail.com>
2017-02-05 14:52:53 +0200

    Add new solution 8kyu: First Step to FizzBuzz

---
+++ AlgoritmsFromCodeWars/8kyu/FizzBuzzFirstStep.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmsFromCodeWars._8kyu
+{
+    public class FizzBuzzFirstStep
+    {
+        /// <summary>
+        /// Description:
+        /// This is the first step to understanding FizzBuzz.
+        /// Your inputs: a positive integer, n, greater than or equal to one. n is provided, you have NO CONTROL over its value.
+        /// Your expected outputs: an array of positive integers from 1 to n
+        /// Your job is to write an algorithm that gets you from the input to the output.
+        /// </summary>
+        public static int[] PreFizz(int n)
+        {
+            return Enumerable.Range(1, n).ToArray();
+        }
+    }
+}
---
+++ TestsAlgoritmsFromCodeWars/8kyu/TestFizzBuzzFirstStep.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlgoritmsFromCodeWars._8kyu;
+using NUnit.Framework;
+
+
+namespace TestsAlgoritmsFromCodeWars._8kyu
+{
+    [TestFixture]
+    class TestFizzBuzzFirstStep
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual(new int[] { 1 }, FizzBuzzFirstStep.PreFizz(1), "Array should be from 1 to 1");
+            Assert.AreEqual(new int[] { 1, 2 }, FizzBuzzFirstStep.PreFizz(2), "Array should be from 1 to 2");
+            Assert.AreEqual(new int[] { 1, 2, 3 }, FizzBuzzFirstStep.PreFizz(3), "Array should be from 1 to 3");
+            Assert.AreEqual(new int[] { 1, 2, 3, 4 }, FizzBuzzFirstStep.PreFizz(4), "Array should be from 1 to 4");
+            Assert.AreEqual(new int[] { 1, 2, 3, 4, 5 }, FizzBuzzFirstStep.PreFizz(5), "Array should be from 1 to 5");
+        }
+    }
+   
+}
