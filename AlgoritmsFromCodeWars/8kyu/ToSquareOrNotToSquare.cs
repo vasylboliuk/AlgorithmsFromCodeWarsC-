4eb6fba2446e1b8c6ea50d60c26c0d6121dda256
vasylboliuk <vbboliuk@gmail.com>
2017-02-05 13:05:51 +0200

    Add new solution kyu8: Square Or Not Square Root

---
+++ AlgoritmsFromCodeWars/8kyu/ToSquareOrNotToSquare.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmsFromCodeWars._8kyu
+{
+    public class ToSquareOrNotToSquare
+    {
+
+        /// <summary>
+        /// Description:
+        /// To square(root) or not to square(root)
+        /// Wite a method, that will get an integer array as parameter and will process every number from this array.
+        /// Return a new array with processing every number of the input-array like this:
+        /// If the number has an integer square root, take this, otherwise square the number.
+        /// [4, 3, 9, 7, 2, 1] -> [2,9,3,49,4,1]
+        /// The input array will always contain only positive numbers and will never be empty or null.
+        /// The input array should not be modified!
+        /// Have fun coding it and please don't forget to vote and rank this kata! :-)
+        /// I have created other katas.Have a look if you like coding and challenges.
+        /// </summary>
+        public static int[] SquareOrSquareRoot(int[] array)
+        {
+            List<int> rootArray = new List<int>();
+            foreach (int i in array)
+            {
+                if (Math.Sqrt(i) % 1 == 0)
+                {
+                    rootArray.Add((int)Math.Sqrt(i));
+                }
+                else
+                {
+                    rootArray.Add(i*i);
+                }
+            }
+            return rootArray.ToArray();
+        }
+    }
+}
---
+++ TestsAlgoritmsFromCodeWars/8kyu/TestToSquareOrNotToSquare.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlgoritmsFromCodeWars._8kyu;
+using NUnit.Framework;
+
+namespace TestsAlgoritmsFromCodeWars._8kyu
+{
+    class TestToSquareOrNotToSquare
+    {
+
+        [Test]
+        public void BasicTests()
+        {
+            var input = new int[] { 4, 3, 9, 7, 2, 1 };
+            var expected = new int[] { 2, 9, 3, 49, 4, 1 };
+            Assert.AreEqual(string.Join(",", expected), string.Join(",", ToSquareOrNotToSquare.SquareOrSquareRoot(input)));
+
+            input = new int[] { 100, 101, 5, 5, 1, 1 };
+            expected = new int[] { 10, 10201, 25, 25, 1, 1 };
+            Assert.AreEqual(string.Join(",", expected), string.Join(",", ToSquareOrNotToSquare.SquareOrSquareRoot(input)));
+
+            input = new int[] { 1, 2, 3, 4, 5, 6 };
+            expected = new int[] { 1, 4, 9, 2, 25, 36 };
+            Assert.AreEqual(string.Join(",", expected), string.Join(",", ToSquareOrNotToSquare.SquareOrSquareRoot(input)));
+        }
+    }
+}
