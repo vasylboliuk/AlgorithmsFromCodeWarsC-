b6b47261e6a65ad2cb76c62322209777ce69684c
vasylboliuk <vbboliuk@gmail.com>
2017-02-06 23:48:31 +0200

    Add new solution 7kyu: Temperature Aanalysis I

---
+++ AlgoritmsFromCodeWars/7kyu/TemperatureAanalysisI.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmsFromCodeWars._7kyu
+{
+    public class TemperatureAanalysisI
+    {
+
+        public static int? LowestTemperature(string t)
+        {
+            if (t == "") return null;
+           return t.Split(' ').Select(x => Int32.Parse(x)).ToList().Min();
+        }
+    }
+}
---
+++ TestsAlgoritmsFromCodeWars/7kyu/TestTemperatureAanalysisI.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlgoritmsFromCodeWars._7kyu;
+using NUnit.Framework;
+
+namespace TestsAlgoritmsFromCodeWars._7kyu
+{
+    [TestFixture]
+    class TestTemperatureAanalysisI
+    {
+
+        [Test]
+        public void MyTest()
+        {
+            Assert.AreEqual(-8, TemperatureAanalysisI.LowestTemperature("-1 50 -4 20 22 -7 0 10 -8"));
+            Assert.AreEqual(10, TemperatureAanalysisI.LowestTemperature("10 50 12 20 22 13 20 100 18"));
+            Assert.AreEqual(-1, TemperatureAanalysisI.LowestTemperature("-1 50 16 20 22 1 0 10 34"));
+            Assert.AreEqual(-2, TemperatureAanalysisI.LowestTemperature("-2 3 -1 12 45 14"));
+            Assert.AreEqual(null, TemperatureAanalysisI.LowestTemperature(""));
+        }
+    }
+}
