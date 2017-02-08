c640b6e6876433b65be7526553e36c5039d7d9bc
vasylboliuk <vbboliuk@gmail.com>
2017-02-08 23:11:54 +0200

    Add new solution 7kyu:  Simple Extension Methods

---
+++ AlgoritmsFromCodeWars/7kyu/SimpleExtensionMethods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmsFromCodeWars._7kyu
+{
+    public static class SimpleExtensionMethods
+    {
+        public static string SayHello(this string str)
+        {
+            return "Hello, " + str + "!";
+        }
+
+        public static string SayGoodbye(this string str)
+        {
+            return "Goodbye, " + str + ". See you again soon!";
+
+        }
+    }
+}
---
+++ TestsAlgoritmsFromCodeWars/7kyu/TestSimpleExtensionMethods.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlgoritmsFromCodeWars._7kyu;
+using NUnit.Framework;
+
+namespace TestsAlgoritmsFromCodeWars._7kyu
+{
+    [TestFixture]
+    class TestSimpleExtensionMethods
+    {
+
+        [Test]
+        public void ExampleTests()
+        {
+            const string name = "Alex";
+            Assert.AreEqual("Hello, Alex!", name.SayHello());
+            Assert.AreEqual("Goodbye, Alex. See you again soon!", name.SayGoodbye());
+        }
+    }
+}
