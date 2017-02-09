ec36fc3f5659b1c5c0152bf42813f4d44d55012b
vasylboliuk <vbboliuk@gmail.com>
2017-02-09 15:23:40 +0200

    Add new solution 7kyu: Sort Numbers

---
+++ AlgoritmsFromCodeWars/7kyu/SortNumbersSolution.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmsFromCodeWars._7kyu
+{
+    public class SortNumbersSolution
+    {
+        public static int[] SortNumbers(int[] nums)
+        {
+            if (nums == null || nums.Length < 1) return new int[] {};
+            Array.Sort(nums);
+
+            return nums;
+        }
+    }
+}
---
+++ TestsAlgoritmsFromCodeWars/7kyu/TestSortNumbers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlgoritmsFromCodeWars._7kyu;
+using NUnit.Framework;
+
+namespace TestsAlgoritmsFromCodeWars._7kyu
+{
+    [TestFixture]
+    class TestSortNumbers
+    {
+        [Test]
+        public void BasicTests()
+        {
+            checkNums(new int[] { 1, 2, 3, 10, 5 }, new int[] { 1, 2, 3, 5, 10 });
+            checkNums(null, new int[] { });
+            checkNums(new int[] { }, new int[] { });
+            checkNums(new int[] { 20, 2, 10 }, new int[] { 2, 10, 20 });
+            checkNums(new int[] { 2, 20, 10 }, new int[] { 2, 10, 20 });
+            checkNums(new int[] { 2, 10, 20 }, new int[] { 2, 10, 20 });
+        }
+
+        private void checkNums(int[] nums, int[] expected)
+        {
+            var actual = SortNumbersSolution.SortNumbers(nums);
+
+            Assert.AreEqual(expected, actual, nums != null ? "{" + string.Join(",", nums) + "}" : "null");
+        }
+    }
+}
