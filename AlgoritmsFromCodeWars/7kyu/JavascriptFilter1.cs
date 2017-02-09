1722e4cfe66c614e596d81ac00f029948e73bf21
vasylboliuk <vbboliuk@gmail.com>
2017-02-09 17:32:03 +0200

    Add new solution 7kyu: Javascript Filter First variant

---
+++ AlgoritmsFromCodeWars/7kyu/JavascriptFilter1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmsFromCodeWars._7kyu
+{
+    public class JavascriptFilter1
+    {
+        /// <summary>
+        /// While developing a website, you detect that some of the members have troubles logging in. 
+        /// Searching through the code you find that all logins ending with a "_" make problems. 
+        /// So you want to write a function that takes an array of pairs of login-names and e-mails, 
+        /// and outputs an array of all login-name, e-mails-pairs from the login-names that end with "_".
+        /// If you have the input-array:
+        /// [ [ "foo", "foo@foo.com" ], [ "bar_", "bar@bar.com" ] ]
+        /// it should output
+        /// [["bar_", "bar@bar.com"] ]
+        /// You have to use the filter-method of Python, which returns each element of the array for which the filter-method returns true.
+        /// </summary>
+        /// <param name="logins"></param>
+        /// <returns></returns>
+        public static string[][] search_names(string[][] logins)
+        {
+            for (int i=0; i< logins.Length; i++)
+            {
+                if (!logins[i].ElementAt(0).EndsWith("_"))
+                {
+                    logins = logins.Where(w => w != logins[i]).ToArray();
+                    i--;
+                }
+            }
+            return logins;
+        }
+    }
+}
---
+++ TestsAlgoritmsFromCodeWars/7kyu/TestJavascriptFilter1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlgoritmsFromCodeWars._7kyu;
+using NUnit.Framework;
+
+namespace TestsAlgoritmsFromCodeWars._7kyu
+{
+    [TestFixture]
+    class TestJavascriptFilter1
+    {
+
+        [Test]
+        public void Test1()
+        {
+            string[][] a = { new[] { "foo", "foo@foo.com" }, new[] { "bar_", "bar@bar.com" } };
+            string[][] b = { new[] { "bar_", "bar@bar.com" } };
+            Assert.That(b, Is.EquivalentTo(JavascriptFilter1.search_names(a)));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            string[][] a = { new[] { "foo_", "foo@foo.com" }, new[] { "bar_", "bar@bar.com" } };
+            string[][] b = { new[] { "foo_", "foo@foo.com" }, new[] { "bar_", "bar@bar.com" } };
+            Assert.That(b, Is.EquivalentTo(JavascriptFilter1.search_names(a)));
+        }
+
+        [Test]
+        public void Test3()
+        {
+            string[][] a = { new[] { "foo", "foo@foo.com" }, new[] { "bar", "bar@bar.com" } };
+            string[] b = new string[0];
+            Assert.That(b, Is.EquivalentTo(JavascriptFilter1.search_names(a)));
+        }
+    }
+}
