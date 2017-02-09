8b1cef3fc2d32f2d693a18a413008fe68007d1ec
vasylboliuk <vbboliuk@gmail.com>
2017-02-09 14:42:28 +0200

    Add new solution 7kyu: Order string by length and desscing for each group

---
+++ AlgoritmsFromCodeWars/7kyu/SuzukiNeedsHelpLiningUpHisStudents.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmsFromCodeWars._7kyu
+{
+    public class SuzukiNeedsHelpLiningUpHisStudents
+    {
+        /// <summary>
+        /// Suzuki needs help lining up his students!
+        /// Today Suzuki will be interviewing his students to ensure they are progressing in their training.
+        /// He decided to schedule the interviews based on the length of the students name in descending order. 
+        /// The students will line up and wait for their turn.
+        /// You will be given a string of student names. Sort them and return a list of names in descending order.
+        /// Here is an example input:
+        /// string = 'Tadashi Takahiro Takao Takashi Takayuki Takehiko Takeo Takeshi Takeshi'
+        /// Here is an example return from your function:
+        /// lst = ['Takehiko',
+        /// 'Takayuki',
+        /// 'Takahiro',
+        /// 'Takeshi',
+        /// 'Takeshi',
+        /// 'Takashi',
+        /// 'Tadashi',
+        /// 'Takeo',
+        /// 'Takao']
+        /// Names of equal length will be returned in reverse alphabetical order (Z->A) such that:
+        /// string = "xxa xxb xxc xxd xa xb xc xd"
+        /// Returns
+        /// ['xxd', 'xxc', 'xxb', 'xxa', 'xd', 'xc', 'xb', 'xa']
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        public static String[] LineupStudents(String a)
+        {
+            string[] arr = a.Split(); // Create an array from string
+            arr = arr.OrderBy(aux => aux.Length).ToArray(); // Order list by string length
+            // Divide all list to part by length and make order for each list
+            List<string> preResultList = new List<string>();
+            List<string> tempList = new List<string>();
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i-1].Length != arr[i].Length)
+                {
+                    tempList.Add(arr[i-1]);
+                    tempList = tempList.OrderBy(c => c).ToList();
+                    preResultList.AddRange(tempList);
+                    tempList.Clear();
+                }
+
+                else { tempList.Add(arr[i-1]);}
+
+                if (arr.Length - 1 == i)
+                {
+                    tempList.Add(arr[i]);
+                    tempList = tempList.OrderBy(c => c).ToList();
+                    preResultList.AddRange(tempList);
+                }
+            }
+            preResultList.Reverse(); // Reverse list
+            string[] resultArr = preResultList.ToArray();
+            return resultArr;
+        }
+
+        public static String[] LineupStudents2(String a)
+        {
+
+            return a.Split(' ').OrderByDescending(person => person.Length).ThenByDescending(person => person).ToArray();
+        }
+
+        public static String[] LineupStudents3(String a)
+        {
+            return a.Split(' ').OrderByDescending(s => s).OrderByDescending(s => s.Length).ToArray();
+        }
+    }
+}
---
+++ TestsAlgoritmsFromCodeWars/7kyu/TestSuzukiNeedsHelpLiningUpHisStudents.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlgoritmsFromCodeWars._7kyu;
+using NUnit.Framework;
+
+namespace TestsAlgoritmsFromCodeWars._7kyu
+{
+    [TestFixture]
+    class TestSuzukiNeedsHelpLiningUpHisStudents
+    {
+
+        [Test]
+        public void BasicTest0()
+        {
+            String students = "xa xc xxb xxa xxc xxd xb xd";
+            String[] expected = { "xxd", "xxc", "xxb", "xxa", "xd", "xc", "xb", "xa" };
+            Assert.AreEqual(expected, SuzukiNeedsHelpLiningUpHisStudents.LineupStudents(students));
+        }
+
+        [Test]
+        public void BasicTest1()
+        {
+            String students = "Tadashi Takahiro Takao Takashi Takayuki Takehiko Takeo Takeshi Takeshi";
+            String[] expected = { "Takehiko", "Takayuki", "Takahiro", "Takeshi", "Takeshi", "Takashi", "Tadashi", "Takeo", "Takao" };
+            Assert.AreEqual(expected, SuzukiNeedsHelpLiningUpHisStudents.LineupStudents(students));
+        }
+        [Test]
+        public void BasicTest2()
+        {
+            String students = "Michio Miki Mikio Minori Minoru Mitsuo Mitsuru Nao Naoki Naoko Noboru Nobu Nobuo Nobuyuki Nori Norio Osamu Rafu Raiden Ringo Rokuro Ronin Ryo Ryoichi Ryota Ryozo Ryuichi Ryuu Saburo Sadao Samuru Satoru Satoshi Seiichi Seiji Senichi Shichiro Shig Shigekazu Shigeo Shigeru Shima Shin Shinichi Shinji Shiro Shoichi Shoji Shuichi Shuji Shunichi Susumu Tadao Tadashi Takahiro Takao Takashi Takayuki Takehiko Takeo Takeshi Takeshi Takumi Tama Tamotsu Taro Tatsuo Tatsuya Teruo Tetsip Tetsuya Tomi Tomio Toru Toshi Toshiaki Toshihiro Toshio Toshiyuki Toyo Tsuneo Tsutomu Tsuyoshi Uyeda Yasahiro Yasuhiro Yasuo Yasushi Yemon Yogi Yoichi Yori Yoshi Yoshiaki Yoshihiro Yoshikazu Yoshimitsu Yoshinori Yoshio Yoshiro Yoshito Yoshiyuki Yuichi Yuji Yuki";
+            String[] expected = {"Yoshimitsu", "Yoshiyuki", "Yoshinori", "Yoshikazu", "Yoshihiro", "Toshiyuki",
+         "Toshihiro", "Shigekazu", "Yoshiaki", "Yasuhiro", "Yasahiro", "Tsuyoshi","Toshiaki",
+         "Takehiko", "Takayuki", "Takahiro", "Shunichi", "Shinichi","Shichiro",
+         "Nobuyuki", "Yoshito", "Yoshiro", "Yasushi", "Tsutomu","Tetsuya", "Tatsuya",
+         "Tamotsu", "Takeshi", "Takeshi", "Takashi","Tadashi", "Shuichi", "Shoichi",
+         "Shigeru", "Senichi", "Seiichi","Satoshi", "Ryuichi", "Ryoichi", "Mitsuru",
+         "Yuichi", "Yoshio","Yoichi", "Tsuneo", "Toshio", "Tetsip", "Tatsuo", "Takumi",
+        "Susumu", "Shinji", "Shigeo", "Satoru", "Samuru", "Saburo","Rokuro", "Raiden",
+        "Noboru", "Mitsuo", "Minoru", "Minori", "Michio", "Yoshi", "Yemon", "Yasuo",
+        "Uyeda", "Toshi", "Tomio","Teruo", "Takeo", "Takao", "Tadao", "Shuji", "Shoji",
+        "Shiro", "Shima", "Seiji", "Sadao", "Ryozo", "Ryota", "Ronin", "Ringo", "Osamu",
+        "Norio", "Nobuo", "Naoko", "Naoki", "Mikio", "Yuki", "Yuji", "Yori", "Yogi",
+        "Toyo", "Toru", "Tomi", "Taro", "Tama", "Shin", "Shig", "Ryuu", "Rafu", "Nori",
+        "Nobu", "Miki", "Ryo", "Nao"};
+            Assert.AreEqual(expected, SuzukiNeedsHelpLiningUpHisStudents.LineupStudents(students));
+        }
+    }
+}
