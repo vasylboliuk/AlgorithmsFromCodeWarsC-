84c65b36fff42938c9fb5306bfd94df8f68d4b4f
vasylboliuk <vbboliuk@gmail.com>
2017-02-10 15:36:14 +0200

    Add new method for LeapYear

--- AlgoritmsFromCodeWars/7kyu/LeapYear.cs
+++ AlgoritmsFromCodeWars/7kyu/LeapYear.cs
@@ -32,5 +32,13 @@ namespace AlgoritmsFromCodeWars._7kyu
             }
             return false;
         }
+
+        public bool IsLeapYear2(int year)
+        {
+            if (year % 400 == 0) return true;
+            if (year % 100 == 0) return false;
+            if (year % 4 == 0) return true;
+            return false;
+        }
     }
 }
