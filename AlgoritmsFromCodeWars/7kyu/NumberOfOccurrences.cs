3cdc6fd21b15a35b83a82e16291b75ea1617b8a2
vasylboliuk <vbboliuk@gmail.com>
2017-02-10 17:01:17 +0200

    Add new solution 7kyu: Number of occurrences

---
+++ AlgoritmsFromCodeWars/7kyu/NumberOfOccurrences.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmsFromCodeWars._7kyu
+{
+    public class NumberOfOccurrencesSolution
+    {
+        /// <summary>
+        /// The numberOfOccurrences function must return the number of occurrences of an element in an array.
+        /// var sample = { 1, 0, 2, 2, 3 };
+        /// NumberOfOccurrences(0, sample) == 1;
+        /// NumberOfOccurrences(4, sample) == 0;
+        /// NumberOfOccurrences(2, sample) == 2;
+        /// NumberOfOccurrences(3, sample) == 1;
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="xs"></param>
+        /// <returns></returns>
+        public int NumberOfOccurrences(int x, int[] xs)
+        {
+            if (xs.Length < 1) return 0;
+            int count = 0;
+            foreach (var i in xs)
+            {
+                if (i == x)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+
+        public int NumberOfOccurrences2(int x, int[] xs)
+        {
+            if (xs.Length < 1) return 0;
+            return xs.Count(el => el == x);
+        }
+
+        public static int NumberOfOccurrences3(int x, int[] xs)
+        {
+            return xs.Where(i => i.Equals(x)).Count();
+        }
+    }
+}
---
+++ TestsAlgoritmsFromCodeWars/7kyu/TestNumberOfOccurrencesSolution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlgoritmsFromCodeWars._7kyu;
+using NUnit.Framework;
+
+namespace TestsAlgoritmsFromCodeWars._7kyu
+{
+    [TestFixture]
+    class TestNumberOfOccurrencesSolution
+    {
+        NumberOfOccurrencesSolution OccurrencesKata = new NumberOfOccurrencesSolution();
+
+        [Test]
+        public void Should_Work_On_Examples()
+        {
+            var example = new int[] { 4, 0, 4 };
+
+            Assert.AreEqual(2, OccurrencesKata.NumberOfOccurrences(4, example));
+
+            Assert.AreEqual(0, OccurrencesKata.NumberOfOccurrences(1, example));
+
+            Assert.AreEqual(1, OccurrencesKata.NumberOfOccurrences(0, example));
+        }
+    }
+}
