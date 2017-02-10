dfd74310074427da98e498a8b00abd7241c5c3e7
vasylboliuk <vbboliuk@gmail.com>
2017-02-10 17:40:44 +0200

    Add new solution 6kyu: Sum all numbers multiple to 3 or 5 to provided value

---
+++ AlgoritmsFromCodeWars/6kyu/SumMultiplesOf3and5.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmsFromCodeWars._6kyu
+{
+    public class SumMultiplesOf3and5
+    {
+        /// <summary>
+        /// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+        /// The sum of these multiples is 23.
+        /// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+        /// Note: If the number is a multiple of both 3 and 5, only count it once.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static int Solution(int value)
+        {
+            int sum = 0;
+            for (int i = 0; i < value; i++)
+            {
+                if (i%3 == 0 || i%5 == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+            return sum;
+        }
+
+        public static int Solution2(int value)
+        {
+            return Enumerable.Range(0, value).Where(x => x%3 == 0 || x%5 == 0).Sum();
+        }
+    }
+}
---
+++ TestsAlgoritmsFromCodeWars/6kyu/TestSumMultiplesOf3and5.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlgoritmsFromCodeWars._6kyu;
+using NUnit.Framework;
+
+namespace TestsAlgoritmsFromCodeWars._6kyu
+{
+    [TestFixture]
+    class TestSumMultiplesOf3and5
+    {
+       SumMultiplesOf3and5 SumMultiplesOf3and5 = new SumMultiplesOf3and5();
+
+
+        [Test]
+        public void Test()
+        {
+            Assert.AreEqual(23, SumMultiplesOf3and5.Solution(10));
+        }
+    }
+}
