031164669500c65b07b02e3e35ea6dbcd69f7a90
vasylboliuk <vbboliuk@gmail.com>
2017-02-10 22:15:03 +0200

    Add new solution 6kyu: Split string

---
+++ AlgoritmsFromCodeWars/6kyu/SplitString.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmsFromCodeWars._6kyu
+{
+    public class SplitString
+    {
+
+        /// <summary>
+        /// Complete the solution so that it splits the string into pairs of two characters. 
+        /// If the string contains an odd number of characters then it should replace the missing second character of
+        ///  the final pair with an underscore ('_').
+        /// Examples:
+        /// SplitString.Solution("abc"); // should return ["ab", "c_"]
+        /// SplitString.Solution("abcdef"); // should return ["ab", "cd", "ef"] 
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public string[] Solution(string str)
+        {
+            if (str.Length%2 != 0) str = str + "_";
+            List<string> strList = new List<string>();
+            int len = str.Length;
+
+            for (int i = 2; i <= len; i=i+2)
+            {
+                strList.Add(str.Substring(i-2, 2));
+            }
+
+            return strList.ToArray();
+        }
+
+        public static string[] Solution2(string str)
+        {
+            if (str.Length % 2 != 0)
+                str += "_";
+            return Enumerable.Range(0, str.Length)
+              .Where(x => x % 2 == 0)
+              .Select(x => str.Substring(x, 2))
+              .ToArray();
+        }
+    }
+}
---
+++ TestsAlgoritmsFromCodeWars/6kyu/TestSplitString.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlgoritmsFromCodeWars._6kyu;
+using NUnit.Framework;
+
+namespace TestsAlgoritmsFromCodeWars._6kyu
+{
+    [TestFixture]
+    class TestSplitString
+    {
+        SplitString SplitString = new SplitString();
+
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual(new string[] { "ab", "c_" }, SplitString.Solution("abc"));
+            Assert.AreEqual(new string[] { "ab", "cd", "ef" }, SplitString.Solution("abcdef"));
+        }
+    }
+}
