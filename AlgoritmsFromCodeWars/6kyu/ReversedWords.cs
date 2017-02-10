49d72f1b59c88556e7b5fba2519382a3b772feb7
vasylboliuk <vbboliuk@gmail.com>
2017-02-10 20:42:04 +0200

    Add new solution 6kyu: Reverse words in sentence

---
+++ AlgoritmsFromCodeWars/6kyu/ReversedWords.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmsFromCodeWars._6kyu
+{
+    public class ReversedWords
+    {
+
+        public string ReverseWords(string str)
+        {
+            return String.Join(" ", str.Split().Reverse());
+        }
+
+        public string ReverseWords2(string str)
+        {
+            string sReturn = "";
+            var split = str.Split(' ');
+            for (int i = split.Length - 1; i >= 0; i--)
+            {
+                sReturn += split[i];
+                if (i > 0)
+                {
+                    sReturn += " ";
+                }
+            }
+            return sReturn;
+        }
+
+    }
+}
---
+++ TestsAlgoritmsFromCodeWars/6kyu/TestReversedWords.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlgoritmsFromCodeWars._6kyu;
+using NUnit.Framework;
+
+namespace TestsAlgoritmsFromCodeWars._6kyu
+{
+    [TestFixture]
+    class TestReversedWords
+    {
+        ReversedWords Kata = new ReversedWords();
+
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual("world! hello", Kata.ReverseWords("hello world!"));
+            Assert.AreEqual("this like speak doesn't yoda", Kata.ReverseWords("yoda doesn't speak like this"));
+            Assert.AreEqual("foobar", Kata.ReverseWords("foobar"));
+            Assert.AreEqual("kata editor", Kata.ReverseWords("editor kata"));
+            Assert.AreEqual("boat your row row row", Kata.ReverseWords("row row row your boat"));
+        }
+    }
+}
