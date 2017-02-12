e83f5ca9b8b7913b41a2b1d9aad7ad3779634d12
vasylboliuk <vbboliuk@gmail.com>
2017-02-12 15:25:18 +0200

    Add new solution 7kyu OOP remove items from list from other list

---
+++ AlgoritmsFromCodeWars/7kyu/KataWithOOPTasks/RemoveAllTheMarkedElementsOfAList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmsFromCodeWars._7kyu.KataWithOOPTasks
+{
+    public  class RemoveAllTheMarkedElementsOfAList
+    {
+        public int[] Remove(int[] integerList, int[] valuesList)
+        {
+            if (integerList.Length<1) return new int[] {};
+            List<int> integers = integerList.ToList();
+            List<int> values = valuesList.ToList();
+
+            integers.RemoveAll(i => values.Contains(i));
+
+            return integers.ToArray();
+        }
+
+        //Works only for non duplicates items in value list
+        public int[] Remove4Variant(int[] integerList, int[] valuesList)
+        {
+            return integerList.ToList().Except(valuesList).ToArray();
+
+        }
+
+        public int[] Remove2Variant(int[] integerList, int[] valuesList)
+        {
+            return integerList.Where(x => !valuesList.Contains(x)).ToArray();
+        }
+
+        public int[] Remove3Variant(int[] integerList, int[] valuesList)
+        {
+            var list = integerList.ToList();
+            foreach (var n in valuesList)
+            {
+                list.RemoveAll(i => i == n);
+            }
+            return list.ToArray();
+        }
+    }
+}
---
+++ TestsAlgoritmsFromCodeWars/7kyu/KataWithOOPTasks/TestRemoveAllTheMarkedElementsOfAList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlgoritmsFromCodeWars._7kyu.KataWithOOPTasks;
+using NUnit.Framework;
+
+namespace TestsAlgoritmsFromCodeWars._7kyu.KataWithOOPTasks
+{
+    [TestFixture]
+    class TestRemoveAllTheMarkedElementsOfAList
+    {
+        RemoveAllTheMarkedElementsOfAList Kata = new RemoveAllTheMarkedElementsOfAList();
+
+        [Test]
+        [TestCase(new int[] { 1, 1, 2, 3, 1, 2, 3, 4 }, new int[] { 1, 3 }, ExpectedResult = new int[] { 2, 2, 4 })]
+        [TestCase(new int[] { 1, 1, 2, 3, 1, 2, 3, 4, 4, 3, 5, 6, 7, 2, 8 }, new int[] { 1, 3, 4, 2 }, ExpectedResult = new int[] { 5, 6, 7, 8 })]
+        [TestCase(new int[] { }, new int[] { 2, 2, 4, 3 }, ExpectedResult = new int[] { })]
+        public int[] FixedTest(int[] integerList, int[] valuesList)
+        {
+            return Kata.Remove(integerList, valuesList);
+        }
+    }
+}
