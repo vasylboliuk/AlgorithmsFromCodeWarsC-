054317248c7c4eb11d1e851fc840f4465525dabd
vasylboliuk <vbboliuk@gmail.com>
2017-02-16 13:51:25 +0200

    Small tests updates

---
+++ TestsAlgoritmsFromCodeWars/7kyu/KataWithOOPTasks/TestTwoFightersOneWinner.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlgoritmsFromCodeWars._7kyu.KataWithOOPTasks;
+using NUnit.Framework;
+
+namespace TestsAlgoritmsFromCodeWars._7kyu.KataWithOOPTasks
+{
+    [TestFixture]
+    class TestTwoFightersOneWinner
+    {
+        private TwoFightersOneWinner TwoFightersOneWinner = new TwoFightersOneWinner();
+        
+           [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual("Lew", TwoFightersOneWinner.DeclareWinner(new Fighter("Lew", 10, 2), new Fighter("Harry", 5, 4), "Lew"));
+            Assert.AreEqual("Harry", TwoFightersOneWinner.DeclareWinner(new Fighter("Lew", 10, 2), new Fighter("Harry", 5, 4), "Harry"));
+            Assert.AreEqual("Harald", TwoFightersOneWinner.DeclareWinner(new Fighter("Harald", 20, 5), new Fighter("Harry", 5, 4), "Harry"));
+            Assert.AreEqual("Harald", TwoFightersOneWinner.DeclareWinner(new Fighter("Harald", 20, 5), new Fighter("Harry", 5, 4), "Harald"));
+            Assert.AreEqual("Harald", TwoFightersOneWinner.DeclareWinner(new Fighter("Jerry", 30, 3), new Fighter("Harald", 20, 5), "Jerry"));
+            Assert.AreEqual("Harald", TwoFightersOneWinner.DeclareWinner(new Fighter("Jerry", 30, 3), new Fighter("Harald", 20, 5), "Harald"));
+        }
+    }
+}
